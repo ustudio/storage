98565df548ddedbfbafd508b666eb4c0c14c04b9
Thomas Stephens <thomas@ustudio.com>
2014-07-24 13:57:46 -0500

    Renaming PyPi package name to object_storage, since it's free on PyPi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as requirements_file:
     requirements = filter(lambda r_striped: r_striped,
                           map(lambda r: r.strip(), requirements_file.readlines()))
 
-setup(name="storage",
+setup(name="object_storage",
       version="0.1",
       description="Python library for accessing files over various file transfer protocols.",
       url="https://github.com/ustudio/storage",
