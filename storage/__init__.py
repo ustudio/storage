e1ac793de99e6f5ba7dac97fb9a27c9683589535
Molly Thurin <mthurin@ustudio.com>
2019-08-20 17:23:37 +0900

    The register_swift_protocol needs to be exposed.

--- storage/__init__.py
+++ storage/__init__.py
@@ -1,7 +1,7 @@
 from storage.storage import get_storage, register_storage_protocol  # noqa: F401
-# from .swift_storage import register_swift_protocol  # noqa: F401
 from storage import local_storage  # noqa: F401
 from storage import swift_storage  # noqa: F401
 from storage import ftp_storage  # noqa: F401
 from storage import s3_storage  # noqa: F401
 from storage import google_storage  # noqa: F401
+from storage.swift_storage import register_swift_protocol  # noqa: F401
