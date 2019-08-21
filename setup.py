fdc2443456abc4f7c5f4a5329c74181d79685785
Molly Thurin <mthurin@ustudio.com>
2019-08-21 22:42:54 +0900

    Adding back the install_requires argument because otherwise this library's requirements won't get installed when you do a pip install object-storage.

--- setup.py
+++ setup.py
@@ -4,8 +4,16 @@ except ImportError:
     from distutils.core import setup
 
 
+install_requires = [
+    "boto3 >= 1.0.1",
+    "google-cloud-storage",
+    "keystoneauth",
+    "python-swiftclient"
+]
+
 setup(name="object_storage",
       version="0.13.0",
       description="Python library for accessing files over various file transfer protocols.",
       url="https://github.com/ustudio/storage",
-      packages=["storage"])
+      packages=["storage"],
+      install_requires=install_requires)
