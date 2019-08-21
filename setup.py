10a459751359408de8aaa837d6e99fab4b48545d
Molly Thurin <mthurin@ustudio.com>
2019-08-21 14:37:42 +0900

    Remove install_requires from setup.py, since they were moved to requirements.txt, and the dev-requirements that were orignally listed in requirements.txt were moved into dev-requirements. Update circle ci config to reflect these changes.

--- setup.py
+++ setup.py
@@ -4,16 +4,8 @@ except ImportError:
     from distutils.core import setup
 
 
-install_requires = [
-    "boto3 >= 1.0.1",
-    "google-cloud-storage",
-    "keystoneauth",
-    "python-swiftclient"
-]
-
 setup(name="object_storage",
-      version="0.12.5",
+      version="0.13.0",
       description="Python library for accessing files over various file transfer protocols.",
       url="https://github.com/ustudio/storage",
-      packages=["storage"],
-      install_requires=install_requires)
+      packages=["storage"])
