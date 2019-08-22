63678e00eff89faaca0b43a57c6fde7e203eb984
Molly Thurin <mthurin@ustudio.com>
2019-08-22 10:43:36 +0900

    Keep both setup.py and requirementstxt, but have requirments.txt contain fixed version of the requirements. This allows CircleCI to use the frozen requirements and have a known, consisten environment every time the tests run, while users of the library can still choose whatever version of the dependencies they want. As per reviewer's request.

--- setup.py
+++ setup.py
@@ -4,16 +4,9 @@ except ImportError:
     from distutils.core import setup
 
 
-install_requires = [
-    "boto3 >= 1.0.1",
-    "google-cloud-storage",
-    "keystoneauth",
-    "python-swiftclient"
-]
-
 setup(name="object_storage",
       version="0.13.0",
       description="Python library for accessing files over various file transfer protocols.",
       url="https://github.com/ustudio/storage",
       packages=["storage"],
-      install_requires=install_requires)
+      install_requires=open("requirements.txt").readlines())
