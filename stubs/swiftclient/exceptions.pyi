c81c8f2352ca4d93d0c465b2d0ea176594e2d9ee
Molly Thurin <mthurin@ustudio.com>
2019-09-10 12:01:22 -0500

    Adding comment to stub file, since the underlying library doesn't provide this attribute, we're "mixing it in" dynamically later.

--- stubs/swiftclient/exceptions.pyi
+++ stubs/swiftclient/exceptions.pyi
@@ -2,6 +2,8 @@ from typing import Optional
 
 
 class ClientException(Exception):
+    # Since underlying library doesn't provide this attribute,
+    # we're "mixing it in" dynamically later.
     do_not_retry: Optional[bool]
     http_status: int
 
