a3ad57814c8ddbb2bcf2f381880cece4cd0bf8b2
Molly Thurin <mthurin@ustudio.com>
2020-04-02 14:08:12 +0900

    Remove some redundancies, as per reviewer's request.
    [PLAY-2375]

--- storage/url_parser.py
+++ storage/url_parser.py
@@ -2,10 +2,11 @@ from urllib.parse import ParseResult
 
 
 def sanitized_uri(parsed_uri: ParseResult) -> str:
+    new_netloc = parsed_uri.hostname
+
     if parsed_uri.port is not None:
-        parsed_url = parsed_uri._replace(
-            netloc=":".join((parsed_uri.hostname, str(parsed_uri.port))))
-    else:
-        parsed_url = parsed_uri._replace(netloc=parsed_uri.hostname)
+        new_netloc = ":".join((new_netloc, str(parsed_uri.port)))
+
+    parsed_url = parsed_uri._replace(netloc=new_netloc)
 
     return parsed_url.geturl()
