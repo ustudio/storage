a59814447b44293412bdcc6f839c42fb2ada8315
Molly Thurin <mthurin@ustudio.com>
2020-04-03 15:12:16 +0900

    Add new function to url_parser that only returns the scheme, hostname, port and path, no query parameters.
    [PLAY-2375]

--- storage/url_parser.py
+++ storage/url_parser.py
@@ -1,6 +1,12 @@
 from urllib.parse import parse_qsl, ParseResult, urlencode
 
 
+def _new_uri(parsed_uri: ParseResult, new_netloc: str, new_query: str) -> ParseResult:
+    return ParseResult(
+        parsed_uri.scheme, new_netloc, parsed_uri.path, parsed_uri.params, urlencode(new_query),
+        parsed_uri.fragment)
+
+
 def remove_user_info(parsed_uri: ParseResult) -> str:
     new_netloc = parsed_uri.hostname
 
@@ -12,8 +18,17 @@ def remove_user_info(parsed_uri: ParseResult) -> str:
     if "download_url_key" in new_query:
         del new_query["download_url_key"]
 
-    new_uri = ParseResult(
-        parsed_uri.scheme, new_netloc, parsed_uri.path, parsed_uri.params, urlencode(new_query),
-        parsed_uri.fragment)
+    new_uri = _new_uri(parsed_uri, new_netloc, new_query)
+
+    return new_uri.geturl()
+
+
+def sanitize_filepath(parsed_uri: ParseResult) -> str:
+    new_netloc = parsed_uri.hostname
+
+    if parsed_uri.port is not None:
+        new_netloc = ":".join((new_netloc, str(parsed_uri.port)))
+
+    new_uri = _new_uri(parsed_uri, new_netloc, {})
 
     return new_uri.geturl()
--- tests/test_url_parser.py
+++ tests/test_url_parser.py
@@ -38,3 +38,21 @@ class TestUrlParser(TestCase):
         sanitized_uri = url_parser.remove_user_info(storage_uri)
 
         self.assertEqual("https://bucket/path/filename?other=parameter", sanitized_uri)
+
+    def test_sanitize_filepath_removes_username_and_password(self) -> None:
+        storage_uri = urlparse("https://username:password@bucket/path/filename")
+        sanitized_uri = url_parser.sanitize_filepath(storage_uri)
+
+        self.assertEqual("https://bucket/path/filename", sanitized_uri)
+
+    def test_sanitize_filepath_does_not_preserves_parameters(self) -> None:
+        storage_uri = urlparse("https://username:password@bucket/path/filename?other=parameter")
+        sanitized_uri = url_parser.sanitize_filepath(storage_uri)
+
+        self.assertEqual("https://bucket/path/filename", sanitized_uri)
+
+    def test_sanitize_filepath_preserves_port_number(self) -> None:
+        storage_uri = urlparse("ftp://username:password@ftp.foo.com:8080/path/filename")
+        sanitized_uri = url_parser.sanitize_filepath(storage_uri)
+
+        self.assertEqual("ftp://ftp.foo.com:8080/path/filename", sanitized_uri)
