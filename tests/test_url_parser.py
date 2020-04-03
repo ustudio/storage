d80a51e0a14bf1e4dbbbbf8dc45d601bba5509e4
Molly Thurin <mthurin@ustudio.com>
2020-04-03 14:41:44 +0900

    Rename library helper tests to reflect the changes made to file.
    [PLAY-2375]

--- tests/test_url_parser.py
+++ tests/test_url_parser.py
@@ -7,32 +7,32 @@ from storage import url_parser
 
 class TestUrlParser(TestCase):
 
-    def test_sanitized_uri_removes_username_and_password(self) -> None:
+    def test_remove_user_info_removes_username_and_password(self) -> None:
         storage_uri = urlparse("https://username:password@bucket/path/filename")
         sanitized_uri = url_parser.remove_user_info(storage_uri)
 
         self.assertEqual("https://bucket/path/filename", sanitized_uri)
 
-    def test_sanitized_uri_preserves_parameters(self) -> None:
+    def test_remove_user_info_preserves_parameters(self) -> None:
         storage_uri = urlparse("https://username:password@bucket/path/filename?other=parameter")
         sanitized_uri = url_parser.remove_user_info(storage_uri)
 
         self.assertEqual("https://bucket/path/filename?other=parameter", sanitized_uri)
 
-    def test_sanitized_uri_preserves_port_number(self) -> None:
+    def test_remove_user_info_preserves_port_number(self) -> None:
         storage_uri = urlparse("ftp://username:password@ftp.foo.com:8080/path/filename")
         sanitized_uri = url_parser.remove_user_info(storage_uri)
 
         self.assertEqual("ftp://ftp.foo.com:8080/path/filename", sanitized_uri)
 
-    def test_sanitized_uri_removes_download_url_key(self) -> None:
+    def test_remove_user_info_removes_download_url_key(self) -> None:
         storage_uri = urlparse(
             "https://username:password@bucket/path/filename?download_url_key=key")
         sanitized_uri = url_parser.remove_user_info(storage_uri)
 
         self.assertEqual("https://bucket/path/filename", sanitized_uri)
 
-    def test_sanitized_uri_removes_download_url_key_and_preserves_parameters(self) -> None:
+    def test_remove_user_info_removes_download_url_key_and_preserves_parameters(self) -> None:
         storage_uri = urlparse(
             "https://username:password@bucket/path/filename?other=parameter&download_url_key=key")
         sanitized_uri = url_parser.remove_user_info(storage_uri)
